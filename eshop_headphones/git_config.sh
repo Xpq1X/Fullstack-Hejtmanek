9e35d4a6b1d40c28e4ee43cc7a50d45023f41cfe
David Hejtmánek <david07@post.cz>
2024-09-23 08:27:44 +0200

    Hello_world

---
+++ eshop_headphones/git_config.sh
@@ -0,0 +1,4 @@
+git config --global user.name "David Hejtmanek"
+git config --global user.email "david07@post.cz"
+echo "Vše ok!"
+#cd eshop_headphones
---
+++ eshop_headphones/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/auth', function () {
+    return view('auth');
+});
