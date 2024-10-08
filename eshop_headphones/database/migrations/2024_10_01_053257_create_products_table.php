497f5c4274477082d309c8cc6e405ffe8824d345
David Hejtmánek <david07@post.cz>
2024-10-08 08:10:43 +0200

    shirahoshi incoming

--- eshop_headphones/database/migrations/2024_10_01_053257_create_products_table.php
+++ eshop_headphones/database/migrations/2024_10_01_053257_create_products_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('size');
+            $table->string('color');
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->integer('stock');
             $table->timestamps();
         });
     }
