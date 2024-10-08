e6d5d8b5f6bca5342c154d336088a2050d0a961a
David Hejtmánek <david07@post.cz>
2024-10-08 08:36:47 +0200

    pls ro ka

--- eshop_headphones/database/seeders/ProductSeeder.php
+++ eshop_headphones/database/seeders/ProductSeeder.php
@@ -12,6 +12,13 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+        for ($i=0; $i < 10; $i++) {         
+            DB::table('products')->insert([
+                'name' => $faker->word,
+                'description' => $faker->sentence,
+                'price' => $faker->randomFloat(2, 50, 1000),
+                'stock' => $faker->numberBetween(1, 100),
+        }
     }
 }
