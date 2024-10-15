dd2e13311b0bb53c77e108d9f96299732a8eba31
David Hejtmánek <david07@post.cz>
2024-10-15 07:22:42 +0200

    Lupit

--- eshop_headphones/database/seeders/DatabaseSeeder.php
+++ eshop_headphones/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,7 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+         User::factory(10)->create();
+         Products::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
--- eshop_headphones/routes/web.php
+++ eshop_headphones/routes/web.php
@@ -16,5 +16,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/products', function () {
+    return view('products.index');
+});
 
 require __DIR__.'/auth.php';
