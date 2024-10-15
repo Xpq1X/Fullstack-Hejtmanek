f6b3285f3a5ddd5941c50d321446aedec3f97103
David Hejtmánek <david07@post.cz>
2024-10-15 07:42:36 +0200

    fixing balkan parents

--- eshop_headphones/database/seeders/DatabaseSeeder.php
+++ eshop_headphones/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
          User::factory(10)->create();
-         ProductFactory::factory(10)->create();
+         Product::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
